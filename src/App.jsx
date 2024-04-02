2cc20c945304d689b5ceccda4e5d3e35ded788fd
Ritik2513 <ritikgupta2513@gmail.com>
2024-04-02 10:47:50 +0530

    input field done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Index from './Components/Index'
 
 function App() {
 
   return (
     <>
-    
+    <div className="bg-black h-screen p-3">
+      <div className="rounded mx-auto max-w-[650px] min-h-[450px] shadow-2xl bg-white">
+        <Index/>
+      </div>
+    </div>
     </>
   )
 }
